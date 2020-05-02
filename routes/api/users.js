0f67a7abe4ccea44be3d76e4cdbb6e72d06db4fb
anggitprayogo <anggitprayogo@gmail.com>
2020-05-02 12:59:12 +0700

    Add register validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-//@route    GET api/users
-//@desc     Test Route
+//@route    POST api/users
+//@desc     Register user
 //@access   Public
-router.get('/', (req, res) => res.send("User Route"))
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email format is invalid').isEmail(),
+    check('password', 'Password min 6 character').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    res.send("User Route")
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express()
 //Connect database
 connectDB()
 
+//init middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send("API RUNNING"))
 
 //define routes

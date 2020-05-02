a158a85704f04a8a0279b749c3551d0b0bcfc495
anggitprayogo <anggitprayogo@gmail.com>
2020-05-02 13:53:39 +0700

    change uri connection to v2 (mongodb atlas) and add functionality to save to mongodb atlas

--- config/db.js
+++ config/db.js
@@ -5,13 +5,14 @@ const db = config.get('mongoURI')
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
 
         console.log('MongoDB connected..')
     } catch (err) {
         console.error(err.message)
-
         process.exit(1)
     }
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
+
+const User = require('../../models/User')
 
 //@route    POST api/users
 //@desc     Register user
@@ -9,13 +13,47 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email format is invalid').isEmail(),
     check('password', 'Password min 6 character').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
 
-    res.send("User Route")
+    const { name, email, password } = req.body
+
+    try {
+
+
+        let user = await User.findOne({ email: email })
+
+        if (user) {
+            res.status(400).json({ errors: [{ msg: 'User is already exists' }] })
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            password,
+            password
+        })
+
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+        //return jsonwebtoken
+        res.send("User registered")
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(errors.message)
+    }
 })
 
 module.exports = router

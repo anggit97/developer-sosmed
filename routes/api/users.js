84547340d53e4ccbb6b7608e5aaa9b246394445c
anggitprayogo <anggitprayogo@gmail.com>
2020-05-02 16:17:50 +0700

    Add jwt creation when registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -22,12 +24,10 @@ router.post('/', [
     const { name, email, password } = req.body
 
     try {
-
-
         let user = await User.findOne({ email: email })
 
         if (user) {
-            res.status(400).json({ errors: [{ msg: 'User is already exists' }] })
+            return res.status(400).json({ errors: [{ msg: 'User is already exists' }] })
         }
 
         const avatar = gravatar.url(email, {
@@ -40,7 +40,7 @@ router.post('/', [
             name,
             email,
             password,
-            password
+            avatar
         })
 
         const salt = await bcrypt.genSalt(10)
@@ -48,11 +48,26 @@ router.post('/', [
 
         await user.save()
 
-        //return jsonwebtoken
-        res.send("User registered")
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err
+
+                return res.status(200).send({ msg: "success register", token: token })
+            }
+        )
+
     } catch (error) {
         console.log(error.message)
-        res.status(500).send(errors.message)
+        res.status(500).json({ errors: [{ msg: 'server error' }] })
     }
 })
 

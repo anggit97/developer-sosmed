9548aed06d05d372b6a76182e56f0abca6f9b44d
anggitprayogo <anggitprayogo@gmail.com>
2020-05-06 19:43:24 +0700

    Add Login Form with its dummy function

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const { email, password } = formData
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = e => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Fragment>
+            <div className="alert alert-danger">
+                Invalid credentials
+            </div>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <a href="register.html">Sign Up</a>
+            </p>
+        </Fragment>
     )
 }
 

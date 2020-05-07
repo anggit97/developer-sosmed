0c2cc7fd1f5ee73ceb22cce4e8ab63b91c466bdd
anggitprayogo <anggitprayogo@gmail.com>
2020-05-07 12:16:23 +0700

    Add Register with redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,10 @@
 import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
 import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
 import PropTypes from 'prop-types'
-import axios from 'axios'
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -22,28 +22,7 @@ const Register = ({ setAlert }) => {
         if (password !== password2) {
             setAlert("Password is not match", "danger", 3000)
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            }
-
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-
-                const body = JSON.stringify(newUser)
-
-                const res = await axios.post('/api/users', body, config)
-
-                console.log(res.data)
-
-            } catch (error) {
-                console.error(error.response.data)
-            }
+            register({ name, email, password })
         }
     }
 
@@ -56,7 +35,7 @@ const Register = ({ setAlert }) => {
                     <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
                     <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -93,6 +72,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register)
+export default connect(null, { setAlert, register })(Register)

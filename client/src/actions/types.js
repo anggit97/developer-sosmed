3d347ee5bc9ecac329c35e10573abfa9324970aa
anggitprayogo <anggitprayogo@gmail.com>
2020-05-08 10:39:20 +0700

    Get profile from api in dashboard

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,3 +11,6 @@ export const USER_LOADED = "USER_LOADED"
 export const AUTH_ERROR = "AUTH_ERROR"
 
 export const LOGOUT = "LOGOUT"
+
+export const GET_PROFILE = "GET_PROFILE"
+export const PROFILE_ERROR = "PROFILE_ERROR"
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import alert from './alerts'
 import auth from './auth'
+import profile from './profile'
 
 export default combineReducers({
     alert,
-    auth
+    auth,
+    profile
 })

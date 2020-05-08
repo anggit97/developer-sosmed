ce06d32d321d4bef3e3bb3440e59a6fc36694446
anggitprayogo <anggitprayogo@gmail.com>
2020-05-08 09:46:55 +0700

    add protecting route component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import PrivateRoute from './components/routing/PrivateRoute'
 
 //Redux
 import { Provider } from 'react-redux'
@@ -12,6 +13,7 @@ import store from './store'
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth'
 import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token)
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>

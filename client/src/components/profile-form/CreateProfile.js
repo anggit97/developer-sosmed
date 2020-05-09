b46e4cc93898bca5462a25a04cba9821b31d5f1c
anggitprayogo <anggitprayogo@gmail.com>
2020-05-09 18:36:12 +0700

    Create Profile Action

--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
 
     const [formData, setFormData] = useState({
         company: '',
@@ -41,6 +43,11 @@ const CreateProfile = props => {
         [e.target.name]: e.target.value
     })
 
+    const onSubmit = e => {
+        e.preventDefault()
+        createProfile(formData, history)
+    }
+
     return (
         <Fragment>
             <h1 className="large text-primary">
@@ -51,7 +58,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
             <small>* = required field</small>
-            <form className="form">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                     <select name="status" value={status} onChange={e => onChange(e)}>
                         <option value="0">* Select Professional Status</option>
@@ -111,12 +118,6 @@ const CreateProfile = props => {
                     <small className="form-text">Tell us a little about yourself</small>
                 </div>
 
-                <div className="my-2">
-                    <button type="button" className="btn btn-light">
-                        Add Social Network Links
-          </button>
-                    <span>Optional</span>
-                </div>
 
                 <div className="my-2">
                     <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
@@ -162,7 +163,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 }
 
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile))

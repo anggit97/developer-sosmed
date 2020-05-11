5f607c3c75a889279c60b8d81c1bfdb64e026236
anggitprayogo <anggitprayogo@gmail.com>
2020-05-11 19:00:48 +0700

    Starting of the profile page

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import EditProfile from './components/profile-form/EditProfile';
 import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profile/Profiles';
+import Profile from './components/profile/Profile';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token)
@@ -42,6 +43,7 @@ const App = () => {
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Spinner from '../layout/Spinner'
+import { getProfileById } from '../../actions/profile'
+
+const Profile = ({
+    getProfileById,
+    profile: { profile, loading },
+    auth,
+    match
+}) => {
+
+    useEffect(() => {
+        getProfileById(match.params.id)
+    }, [getProfileById])
+
+    return <Fragment>
+        {profile === null || loading ? <Spinner /> : <Fragment>Profile</Fragment>}
+
+        <Fragment>
+            <Link className="btn btn-light" to="/profiles">Back To Profiles</Link>
+            {auth.isAuthenticated && auth.loading === false &&
+                auth.user._id === match.params.id &&
+                (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
+        </Fragment>
+    </Fragment>
+}
+
+Profile.propTypes = {
+    getProfileById: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+    profile: state.auth,
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { getProfileById })(Profile)

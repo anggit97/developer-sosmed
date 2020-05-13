9708f55aa2513f83061b67207beb397c1d71c598
anggitprayogo <anggitprayogo@gmail.com>
2020-05-13 19:09:12 +0700

    Display github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 
 const Profile = ({
     getProfileById,
@@ -63,6 +64,11 @@ const Profile = ({
                                 ) : (<h4>No Education</h4>)
                             }
                         </div>
+
+
+                        {profile.githubusername && (
+                            <ProfileGithub username={profile.githubusername} />
+                        )}
                     </div>
                 </Fragment>
         }

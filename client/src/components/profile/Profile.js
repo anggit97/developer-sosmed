85552971fb3c4b8e8059245b8f19a8dcf593faa7
anggitprayogo <anggitprayogo@gmail.com>
2020-05-13 18:48:00 +0700

    Add Profile Education and Profile Experience

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions/profile'
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 
 const Profile = ({
     getProfileById,
@@ -32,6 +34,35 @@ const Profile = ({
                     <div class="profile-grid my-1">
                         <ProfileTop profile={profile} />
                         <ProfileAbout profile={profile} />
+
+                        <div class="profile-exp bg-white p-2">
+                            <h2 class="text-primary">Experience</h2>
+                            {
+                                profile.experience.length > 0 ? (
+                                    <Fragment>
+                                        {profile.experience.map(experience => (
+                                            <ProfileExperience experience={experience} key={experience._id} />
+                                        ))}
+                                    </Fragment>
+                                ) : (<h4>No Experience</h4>)
+                            }
+                        </div>
+
+
+                        <div class="profile-edu bg-white p-2">
+                            <h2 class="text-primary">Education</h2>
+                            {
+                                profile.education.length > 0 ? (
+                                    <Fragment>
+                                        {
+                                            profile.education.map(education => (
+                                                <ProfileEducation education={education} key={education._id} />
+                                            ))
+                                        }
+                                    </Fragment>
+                                ) : (<h4>No Education</h4>)
+                            }
+                        </div>
                     </div>
                 </Fragment>
         }
